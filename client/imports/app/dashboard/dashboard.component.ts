ccf329098bab2e816b1235e2ad83b9960cffd6ef
Mihir Jain <mihir.jain@smartdatainc.net>
2017-05-09 16:29:58 +0530

    change in upload picture function

--- client/imports/app/dashboard/dashboard.component.ts
+++ client/imports/app/dashboard/dashboard.component.ts
@@ -140,7 +140,7 @@ export class DashboardComponent extends MeteorComponent implements OnInit, After
            name: res.name
       };
       let userData = {
-        "profile.image": userImage;
+        "profile.image": userImage
       };
       this.call("users.update", userData, (err, res) => {
         if (err) {
